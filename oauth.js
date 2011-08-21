83500a3a8b506b1d4869ed725956432eb8f74601
Pat Patterson <ppatterson@salesforce.com>
2011-08-20 22:40:01 -0700

    Refactored, added operations, test page

---
+++ oauth.js
@@ -0,0 +1,70 @@
+var rest = require('restler');
+
+exports.refresh = function refresh(options) {
+    console.log('refresh');
+    rest.post(options.oauth.loginServer+'/services/oauth2/token', {
+        data: { 
+            grant_type: 'refresh_token',
+            client_id: options.oauth.clientId,
+            client_secret: options.oauth.clientSecret,
+            refresh_token: options.oauth.refresh_token
+        },
+    }).on('complete', function(data, response) {
+      if (response.statusCode == 200) {
+          options.callback(data);
+      }
+    }).on('error', function(e) {
+		  console.error(e);
+	});    
+}
+
+exports.oauth = function oauth(options) {
+    var loginServer = options.loginServer || 'https://login.salesforce.com/',
+        clientId = options.clientId,
+        clientSecret = options.clientSecret,
+        redirectUri = options.redirectUri;
+    
+    return function oauth(req, res, next){
+        console.log('oauth');
+        console.log('url :'+req.url);
+        if (req.session.oauth) {
+            // We're done
+            req.oauth = req.session.oauth;
+            req.oauth.loginServer = loginServer;
+            req.oauth.clientId = clientId;
+            req.oauth.clientSecret = clientSecret;
+            console.log(req.session.oauth);
+            next();
+        } else if (req.query.code){
+            // Callback from the Authorization Server
+            console.log('code: '+req.query.code);
+            
+            rest.post(loginServer+'/services/oauth2/token', {
+                data: { 
+                    code: req.query.code,
+                    grant_type: 'authorization_code',
+                    client_id: clientId,
+                    redirect_uri: redirectUri,
+                    client_secret: clientSecret
+                },
+            }).on('complete', function(data, response) {
+              if (response.statusCode == 200) {
+                req.session.oauth = data;
+                state = req.session.oauth_state;
+                delete req.session.oauth_state;
+        		res.redirect(state);
+              }
+            }).on('error', function(e) {
+    			  console.error(e);
+    		});
+        } else {
+            // We have nothing - redirect to the Authorization Server
+            req.session.oauth_state = req.url;
+    	    var oauthURL = loginServer + "/services/oauth2/authorize?response_type=code&" +
+    	        "client_id=" + clientId + "&redirect_uri=" + redirectUri + "&display=touch";
+            console.log('redirecting: '+oauthURL);
+    		res.redirect(oauthURL);  // Redirect to salesforce.com
+    		res.end();            
+        }
+    };
+};
--- rest.js
+++ rest.js
@@ -1,331 +1,209 @@
-var http = require('https');
-var url = require('url');
-var app;
+var rest = require('restler');
 
-var clientId, clientSecret, redirectUri = "https://login.salesforce.com/services/oauth2/success";
-var authenticated = false;
-var loginUrl = "https://login.salesforce.com";
-var refreshToken, accessToken, apiVersion = "22.0";
-var instanceUrl, id, issuedAt, signature = null;
-
-exports.setOptions = function setOptions(options) {
-	open(options.clientId, options.redirectUri, options.version, options.clientSecret, options.hostApp);
-};
-
-var open = function(cid, redirUri, version, csecret, hostApp) {
-	clientId = cid;
-	clientSecret = csecret;
-	if (redirUri) redirectUri = redirUri;
-	if (version) apiVersion = version;
-	hostApp.get('/token', handleToken);
-};
-
-function handleToken(req, clientResponse) {
-	if (url.parse(req.url, true).query.code) {
-		var code = url.parse(req.url, true).query.code;
-		var state = url.parse(req.url, true).query.state;
-		
-		// Got authorization token, use it to get the access token
-		var post_data = 'code='+code+'&grant_type=authorization_code&client_id='+clientId+'&redirect_uri='+escape(redirectUri)+'&client_secret='+clientSecret;
-		var options = {
-			host: "login.salesforce.com",
-			path: '/services/oauth2/token',
-			method: 'POST',
-			headers: {
-				'host': "login.salesforce.com",
-				'Content-Length': post_data.length,
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Accept':'application/jsonrequest',
-				'Cache-Control':'no-cache,no-store,must-revalidate'
+function request(options) {
+	// TODO - API version
+	var restUrl = options.oauth.instance_url + '/services/data' + options.path;
+	console.log("\n\nrest.request - method: " + options.method + " restUrl: " + restUrl + ", data: " + options.data);
+	
+	rest.request(restUrl, {
+	    method: options.method,
+	    data: options.data,
+	    headers: {
+	        'Accept':'application/json',
+	        'Authorization':'OAuth ' + options.oauth.access_token,
+	        'Content-Type': 'application/json',
+	        'Content-Length': (options.data) ? options.data.length : undefined
+	    }
+    }).on('complete', function(data, response) {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+			console.log("REST Response: " + data);
+			if (data.length == 0) {
+				options.callback();
+			} else {
+				options.callback(JSON.parse(data));
 			}
-		};
-		authenticated = true;
-		
-		var xhr = http.request(options, function(res) {
-			var responseData;
-			
-			res.on('data', function(data) {
-				setOAuthData(JSON.parse(data));
-				responseData = data;
-			});
-
-			res.on('end', function(d) {
-			  	redirectUser(clientResponse, responseData, state);
-			});
-
-		}).on('error', function(e) {
-			  console.error(e);
-		});
-
-		xhr.write(post_data);
-		xhr.end();
-		
-	}
-};
-
-function redirectUser(res, oauthResponse, state) {
-   	res.setHeader('Set-Cookie', 
-			[ 
-			'refresh_token=' + escape(refreshToken),
-			'access_token=' + escape(accessToken),
-   	    	'instance_url=' + instanceUrl,
-			'id=' + id,
-			'issued_at=' + issuedAt,
-			'signature=' + signature
-			]
-	); 
-   	res.writeHead(301, {'Location' : state, 'Cache-Control':'no-cache,no-store,must-revalidate'});
-  	res.end();
-}	
-
-function haveAuthenticated(req, res, callback) {
-	// Look for oauth data in cookies
-	var cookies = {};
-	req.headers.cookie && req.headers.cookie.split(';').forEach(function( cookie ) {
-		var parts = cookie.split('=');
-		if (parts[0].trim().substring(0, 3) == "dfc") {
-			cookies[ parts[ 0 ].trim() ] = unescape(( parts[ 1 ] || '' ).trim());
-		}
+        }
+    }).on('error', function(data, response) {
+		  console.error(data);
+		  if (response.statusCode == 401) {
+		      // Session expired or invalid
+		      if ( options.retry || ! options.refresh ) {
+		          // We already tried, or there is no refresh callback
+		          options.error(data, response);
+		      } else {
+		          // We use a refresh callback from options to decouple
+		          // rest from oauth
+		          options.refresh(function(oauth){
+		              options.oauth.access_token = oauth.access_token;
+        		      options.retry = true;
+        		      request(options);		              
+		          });
+		      }
+		  }
 	});
+}
 
-	if (!cookies.hasOwnProperty("access_token")) {
-		accessToken = null;
-		odataString = null;
-	    var oauthURL = loginUrl + "/services/oauth2/authorize?response_type=code&" +
-	        "client_id=" + clientId + "&redirect_uri=" + redirectUri + "&display=touch&state=/accounts";
-		res.redirect(oauthURL);  // Redirect to salesforce.com
-		res.end();
-	} else {
-		setOAuthData(cookies);
-		authenticated = true;
-		callback(req, res);
-	}
+exports.api = function api(options) {
+    var apiVersion = options.apiVersion || '22.0';
+    var oauth = options.oauth;
+    
+    return {
+        versions: function versions(callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/',
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        },
+
+        resources: function resources(callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/',
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        },
+        
+        describeGlobal: function describeGlobal(callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/sobjects/',
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        },
+        
+        metadata: function metadata(objtype, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/sobjects/' + objtype + '/',
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        },
+        
+        describe: function describe(objtype, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/sobjects/' + objtype + '/describe/',
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        },
+        
+        create: function create(objtype, fields, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/sobjects/' + objtype + '/',
+        		callback: callback,
+        		error: error,
+        		method: 'POST',
+        		data: JSON.stringify(fields)
+        	}
+        	request(options);
+        },
+        
+        retrieve: function retrieve(objtype, id, fields, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/sobjects/' + objtype + '/' + id
+                    + '?fields=' + fields,
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        },
+        
+        upsert: function upsert(objtype, externalIdField, externalId, fields, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/sobjects/' + objtype + '/' + externalIdField + '/' + externalId,
+        		callback: callback,
+        		error: error,
+        		method: 'PATCH',
+        		data: JSON.stringify(fields)
+        	}
+        	request(options);
+        },
+        
+        update: function update(objtype, id, fields, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/sobjects/' + objtype + '/' + id,
+        		callback: callback,
+        		error: error,
+        		method: 'PATCH',
+        		data: JSON.stringify(fields)
+        	}
+        	request(options);
+        },
+        
+        del: function del(objtype, id, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/sobjects/' + objtype + '/' + id,
+        		callback: callback,
+        		error: error,
+        		method: 'DELETE',
+        	}
+        	request(options);
+        },
+        
+        search: function search(sosl, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/search/?q=' + escape(sosl),
+        		callback: callback,
+        		error: error
+        	}
+        	request(options);
+        },
+        
+        query: function query(soql, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/query/?q=' + escape(soql),
+        		callback: callback,
+        		error: error
+        	}
+        	request(options);
+        },
+        
+        recordFeed: function recordFeed(id, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/chatter/feeds/record/' + id + '/feed-items',
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        },
+        
+        newsFeed: function newsFeed(id, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/chatter/feeds/news/' + id + '/feed-items',
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        },
+        
+        profileFeed: function profileFeed(id, callback, error) {
+        	var options = {
+        	    oauth: oauth,
+        		path: '/v' + apiVersion + '/chatter/feeds/user-profile/' + id + '/feed-items',
+        		callback: callback,
+        		error: error,
+        	}
+        	request(options);
+        }        
+    };
 };
-
-function setOAuthData(oauthdata) {
-	accessToken = oauthdata["access_token"];
-	refreshToken = oauthdata["refresh_token"];
-	instanceUrl = oauthdata["instance_url"];
-	id = oauthdata["id"];
-	issuedAt = oauthdata["issued_at"];
-	signature = oauthdata["signature"];
-};
-
-/*refreshAccessToken:function(callback, error) {
-	Ti.API.debug("Got to the refreshAccessToken function...");
-   	var xhr = Titanium.Network.createHTTPClient();
-   	
-	xhr.onload = function() {
-		Ti.API.debug(xhr.responseData);
-		var jsonResponse = JSON.parse(xhr.responseData);
-		jsonResponse["refresh_token"] = fa.refreshToken;
-		fa.setOAuthData(jsonResponse);
-		callback(xhr.responseData);
-	};
-	
-	xhr.onerror = function(e) { 
-		Ti.API.info("ERROR " + e.error); 
-		alert(e.error); 
-	 }; 
-	 	
-	var turl = "http://www.postbin.org/zu59sx";
-    var url = fa.loginUrl + '/services/oauth2/token';
-	
-	Ti.API.debug("Refresh token url: " + url);
-	
-	xhr.open("POST", url);
-	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	
-	Ti.API.debug("Grant: " + 'grant_type=refresh_token&client_id=' + fa.clientId + '&refresh_token=' + fa.refreshToken + "\n\nURL: " + url);
-	
-	xhr.send( 'grant_type=refresh_token&client_id=' + fa.clientId + '&refresh_token=' + fa.refreshToken );
-};
-*/
-function makeRestCall(options) {
-	var path = options.path, callback = options.callback, error = options.errorCallback;
-	var method = options.method, oReq = options.origReq, oRes = options.origRes;
-	var payload = options.payload, retry = options.retry;	
-	
-	if (authenticated === true) {
-		var restUrl = instanceUrl + '/services/data' + path;
-		console.log("\n\nmakeRestCall.restUrl: " + restUrl);
-		
-		var options = {
-			host: url.parse(instanceUrl).host,
-			path: '/services/data' + path,
-			method: method,
-			headers:  {
-				'host': url.parse(instanceUrl).host,
-				'Content-Type': 'application/x-www-form-urlencoded',
-				'Accept':'application/json',
-				'Cache-Control':'no-cache,no-store,must-revalidate',
-				'Authorization':'OAuth ' + accessToken
-			}
-		};
-
-
-		var xhrReq = http.request(options, function(xhr) {
-			var restData = "";
-			
-			if (xhr.statusCode != 200) {
-				if (!refreshToken || retry) {
-					// This is an error, we have retried and still don't have a token
-				} else {
-					if (xhr.statusCode == 401) {
-						// Token is not valid, need to grab a token via refresh and then
-						// retry the call
-						refreshAccessToken(function(oauthResponse) {
-							options.retry = true;
-							makeRestCall(options);
-						}, error);
-					} else {
-						// Not a 401, but not a success either
-						// Need to bubble the error up
-						console.log("\n\nXHR ERROR: " + xhr.statusCode);
-					}
-				} 
-			}
-
-			xhr.on('data', function(d) {
-				restData += d;
-			});
-		
-			xhr.on('end', function(restResponse) {  
-				console.log("REST Response: " + restData);
-				if (restData.length == 0) {
-					callback("");
-				} else {
-					var data = JSON.parse(restData);
-				}
-				callback(data);
-			});
-			
-			xhr.on('error',function(e) { 
-				console.log("XHR, error handler..." + 
-					"\nDbDotCom.REST.OAuth.refreshToken: " + refreshToken + 
-					"\nretry: " + retry +
-					"\n e: " + e.toString() +
-					"\nXHR status: " + xhr.status);
-			});
-		});
-		
-		console.log("Rest url: " + restUrl);
-	
-		if (payload) {
-			xhrReq.write(payload);
-		}
-		xhrReq.end();
-	} else {
-		// Got to go get tokens
-		haveAuthenticated(oReq, oRes, function(){
-			makeRestCall(options);
-		});
-	}
-};
-
-exports.query = function query(soql, req, res, callback, error) {
-	var options = {
-		path:'/v' + apiVersion + '/query/?q=' + escape(soql),
-		callback:callback,
-		errorCallback:error,
-		method:"GET",
-		origReq:req,
-		origRes:res
-	}
-	makeRestCall(options);
-};
-
-/*
-setRefreshToken : function(refreshToken) {
-    fa.refreshToken = refreshToken;
-};
-
-refreshAccessToken : function(callback, error) {
-	fa.refreshAccessToken(callback, error);
-};
-*/
-// Need to verify this one as far as options are concerned
-//versions : function(callback, error) {
-//	makeRestCall('.json', callback, error);
-//};
-
-exports.resources = function resources(req, res, callback, error) {
-	var options = {
-		path:'/v' + apiVersion + '/',
-		callback:callback,
-		errorCallback:error,
-		method:"GET",
-		origReq:req,
-		origRes:res
-	}
-	makeRestCall(options);
-};
-/*	,
-    describeGlobal : function(callback, error) {
-        fa.makeRestCall('/v' + fa.apiVersion + '/sobjects/', callback, error);
-    }
-	,
-    metadata : function(objtype, callback, error) {
-        fa.makeRestCall('/v' + fa.apiVersion + '/sobjects/' + objtype + '/'
-        , callback, error);
-    }
-	,
-    describe : function(objtype, callback, error) {
-        fa.makeRestCall('/v' + fa.apiVersion + '/sobjects/' + objtype
-        + '/describe/', callback, error);
-    }
-	,
-    create : function(objtype, fields, callback, error) {
-        fa.makeRestCall('/v' + fa.apiVersion + '/sobjects/' + objtype
-        , callback, error, "POST", JSON.stringify(fields));
-    }
-	,
-    retrieve : function(objtype, id, fieldlist, callback, error) {
-        fa.makeRestCall('/v' + fa.apiVersion + '/sobjects/' + objtype + '/' + id
-        + '?fields=' + fieldlist, callback, error);
-    }
-	,
-    update : function(objtype, id, fields, callback, error) {
-        fa.makeRestCall('/v' + fa.apiVersion + '/sobjects/' + objtype + '/' + id
-        , callback, error, "PATCH", JSON.stringify(fields));
-    }
-	,
-    del : function(objtype, id, callback, error) {
-        fa.makeRestCall('/v' + fa.apiVersion + '/sobjects/' + objtype + '/' + id
-        , callback, error, "DELETE");
-    }
-	,
-    search : function(sosl, callback, error) {
-        fa.makeRestCall('/v' + fa.apiVersion + '/search/?q=' + escape(sosl)
-        , callback, error);
-    }
-    ,
-    recordFeed : function(recordId, callback, error) {
-    	fa.makeRestCall('/v' + fa.apiVersion + '/chatter/feeds/record/' + recordId + '/feed-items', 
-    		callback, error);
-    }
-	,
-    newsFeed : function(recordId, callback, error) {
-    	fa.makeRestCall('/v' + fa.apiVersion + '/chatter/feeds/news/' + recordId + '/feed-items', 
-    		callback, error);
-    }
-	,
-    profileFeed : function(recordId, callback, error) {
-    	fa.makeRestCall('/v' + fa.apiVersion + '/chatter/feeds/user-profile/' + recordId + '/feed-items', 
-    		callback, error);
-    }
-};
-
-exports.ForceOAuth = fa;
-*/
-
-
-
-
-
-
-
-
-
-
-
